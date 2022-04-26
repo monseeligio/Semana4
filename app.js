7d4c20dd6048ddc7a7c6f77fc8fc31ee34d988a9
monseeligio <monserrateligio@gmail.com>
2022-04-25 21:17:33 -0500

    Automatizando pruebas

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const fs = require("fs");
 const rawdata = fs.readFileSync("explorers.json");
 const explorers = JSON.parse(rawdata);
 
+console.log("Hey!. i'm here")
+//console.log(explorers)
+//console.log(rawdata)
 // Part 2: Get the quantity of explorers names in node
 const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
 //console.log(explorersInNode.length)
@@ -59,5 +62,12 @@ const assignFizzBuzzTrick = function(explorer){
 };
 
 const explorersInNodeAndFizzBuzzTrick = explorersInNode.map((explorer) => assignFizzBuzzTrick(explorer));
-
-// Part 8: Get a list of the explorers in node, if the score is divisible by 5 and 3, set the property trick and the value FIZZBUZZ, if is just divisible by 5 set the property trcik and the value BUZZ, if is just divisible by 3 set the property trick and the value FIZZ, otherwise set the property trick and the score value. TODO
+/*
+// Part 8: Get a list of the explorers in node,
+if the score is divisible by 5 and 3,
+set the property trick and the value FIZZBUZZ,
+if is just divisible by 5 set the property
+trcik and the value BUZZ, if is just divisible
+by 3 set the property trick and the value FIZZ,
+otherwise set the property trick and the score value. TODO
+*/
